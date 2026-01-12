e599a788df80786c4ed883688c2e1612f8de180f
V_lastelin <vladbogun@gmail.com>
2026-01-13 03:04:53 +0400

    Fix import order in StoryLogRepository

--- src/main/java/com/bogun/prado_bot/story/StoryLogRepository.java
+++ src/main/java/com/bogun/prado_bot/story/StoryLogRepository.java
@@ -1,11 +1,11 @@
 package com.bogun.prado_bot.story;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-import java.util.List;
-
 public interface StoryLogRepository extends JpaRepository<StoryLogEntity, Long> {
     List<StoryLogEntity> findBySessionIdOrderByStepAsc(long sessionId);
 }
